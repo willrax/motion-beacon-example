3388be83954b3027516f3afe7c99397e71f4f944
Will Raxworthy <git@willrax.com>
2013-11-21 20:58:04 +1100

    Improve ranging of beacons.

--- app/finder_view_controller.rb
+++ app/finder_view_controller.rb
@@ -1,13 +1,51 @@
 class FinderViewController < UIViewController
-  UUID = NSUUID.alloc.initWithUUIDString("7cbff8c6-4d84-4e8c-8703-377e03d4f69f")
+  UUID = NSUUID.alloc.initWithUUIDString("2E64F516-D4E9-429C-9C62-A129A5FB1D02")
 
   def viewDidLoad
     super
     self.view.backgroundColor = UIColor.whiteColor
     self.title = "Searching..."
     createLabel
-    @beaconRegion = CLBeaconRegion.alloc.initWithProximityUUID(UUID, major: 1, minor: 1, identifier: "com.willrax.region")
+    @beaconRegion = CLBeaconRegion.alloc.initWithProximityUUID(UUID, major: 0, minor: 0, identifier: "com.willrax.region")
+    @beaconRegion.notifyOnEntry = true
+    @beaconRegion.notifyOnExit = true
+    @beaconRegion.notifyEntryStateOnDisplay = true
+
     locationManager.startMonitoringForRegion(@beaconRegion)
+    locationManager.requestStateForRegion(@beaconRegion)
+  end
+
+  def locationManager(manager, didDetermineState: state, forRegion: region)
+    if state == CLRegionStateInside
+      locationManager.startRangingBeaconsInRegion(region)
+    end
+  end
+
+  def locationManager(manager, didEnterRegion: region)
+    if region.isKindOfClass(CLBeaconRegion)
+      locationManager.startRangingBeaconsInRegion(region)
+    end
+  end
+
+  def locationManager(manager, didExitRegion: region)
+    if region.isKindOfClass(CLBeaconRegion)
+      locationManager.stopRangingBeaconsInRegion(region)
+    end
+  end
+
+  def locationManager(manager, didRangeBeacons: beacons, inRegion: region)
+    beacon = beacons.last
+
+    if beacon
+      @beaconLabel.text = case beacon.proximity
+                          when CLProximityUnknown then "Unknown"
+                          when CLProximityFar then "Far"
+                          when CLProximityNear then "Near"
+                          when CLProximityImmediate then "Immediate"
+                          else
+                            "Nothing"
+                          end
+    end
   end
 
   def createLabel
@@ -22,20 +60,6 @@ class FinderViewController < UIViewController
       layout.subviews "beacon" => @beaconLabel
       layout.vertical "|-space-[beacon]"
       layout.horizontal "|-[beacon]-|"
-    end
-  end
-
-  def locationManager(manager, didEnterRegion: region)
-    locationManager.startRangingBeaconsInRegion region
-  end
-
-  def locationManager(manager, didRangeBeacons: beacons, inRegion: region)
-    beacon = beacons.last
-
-    if beacon.proximity == CLProximityImmediate
-      @beaconLabel.text = "Beacon Found"
-    else
-      @beaconLabel.text = "No Beacon"
     end
   end
 
